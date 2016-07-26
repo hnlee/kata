0fcfe1b6581d3c45f44086763436467264ab7e1f
Hana Lee <hanalee07@gmail.com>
2016-07-26 14:19:13 -0500

    Refactor outer while loop into for loop

--- primefactors/v02/PrimeFactors.java
+++ primefactors/v02/PrimeFactors.java
@@ -4,12 +4,10 @@ public class PrimeFactors {
 
     public static ArrayList<Integer> generate(int number) {
         ArrayList<Integer> primes = new ArrayList<Integer>();
-        int factor = 2;
-        while (number > 1) {
-            for (; number % factor == 0; number /= factor){
+        for (int factor = 2; number > 1; factor++) {
+            for (; number % factor == 0; number /= factor) {
                 primes.add(factor);
             }
-            factor++; 
         }
         return primes;
     }   
