b65aa6ff91f18c35ece145574841979dc94b21b9
Hana Lee <hanalee07@gmail.com>
2016-07-27 14:32:04 -0500

    Refactor with a boolean to track if a roll results in a spare

--- bowlinggame/v00/Game.java
+++ bowlinggame/v00/Game.java
@@ -11,8 +11,7 @@ public class Game {
         int score = 0;
         int frameIndex = 0;
         for (int frame = 0; frame < 10; frame++) {
-            if (rolls[frameIndex] + 
-                rolls[frameIndex + 1] == 10) {
+            if (isSpare(frameIndex)) {
                 score += 10 + rolls[frameIndex + 2];
             } else {
                 score += rolls[frameIndex] +
@@ -21,6 +20,11 @@ public class Game {
             frameIndex += 2;
         }
         return score;
+    }
+
+    private boolean isSpare(int frameIndex) {
+        return rolls[frameIndex] + 
+               rolls[frameIndex + 1] == 10;
     }
 
 }
