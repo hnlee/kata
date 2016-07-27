01b622e8a4e0d2e5bb53f21c9f66378a85b01985
Hana Lee <hanalee07@gmail.com>
2016-07-27 14:42:42 -0500

    Refactor to use boolean to keep track of whether there was a strike

--- bowlinggame/v00/Game.java
+++ bowlinggame/v00/Game.java
@@ -11,7 +11,7 @@ public class Game {
         int score = 0;
         int frameIndex = 0;
         for (int frame = 0; frame < 10; frame++) {
-            if (rolls[frameIndex] == 10) {
+            if (isStrike(frameIndex)) {
                 score += 10 + 
                          rolls[frameIndex + 1] +
                          rolls[frameIndex + 2];
@@ -26,6 +26,10 @@ public class Game {
             }
         }
         return score;
+    }
+
+    private boolean isStrike(int frameIndex) {
+        return rolls[frameIndex] == 10;
     }
 
     private boolean isSpare(int frameIndex) {
