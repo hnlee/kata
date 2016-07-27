b901b84fe165bd442e77daaaa635a30bfca63a57
Hana Lee <hanalee07@gmail.com>
2016-07-27 11:39:39 -0500

    Write assert statement in test and make a score() method

--- bowlinggame/v00/BowlingGameTest.java
+++ bowlinggame/v00/BowlingGameTest.java
@@ -9,6 +9,7 @@ public class BowlingGameTest {
         for (int i = 0; i < 20; i++) {
             g.roll(0);
         }
+        assertEquals(0, g.score());
     }
 
 }
--- bowlinggame/v00/Game.java
+++ bowlinggame/v00/Game.java
@@ -3,4 +3,8 @@ public class Game {
     public void roll(int pins) {
     }
 
+    public int score() {
+        return 0;
+    }
+
 }
