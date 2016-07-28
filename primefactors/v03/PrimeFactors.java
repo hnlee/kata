f27ece24ff520116b111be5379ef44d912a9a724
Hana Lee <hanalee07@gmail.com>
2016-07-28 16:05:37 -0500

    Write test for factoring 2 and have method return 2

--- primefactors/v03/PrimeFactors.java
+++ primefactors/v03/PrimeFactors.java
@@ -3,7 +3,9 @@ import java.util.ArrayList;
 public class PrimeFactors {
 
     public static ArrayList<Integer> generate(int number) {
-        return new ArrayList<Integer>();
+        ArrayList<Integer> primes = new ArrayList<Integer>();
+        primes.add(2);
+        return primes;
     }
 
 }
