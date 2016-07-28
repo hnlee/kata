21953387c0529bfbe2af8c5311f28c7e380f9767
Hana Lee <hanalee07@gmail.com>
2016-07-28 16:20:55 -0500

    Refactor to make two while loops into two for loops

--- primefactors/v03/PrimeFactors.java
+++ primefactors/v03/PrimeFactors.java
@@ -4,13 +4,10 @@ public class PrimeFactors {
 
     public static ArrayList<Integer> generate(int number) {
         ArrayList<Integer> primes = new ArrayList<Integer>();
-        int prime = 2;
-        while (number > 1) {
-            while (number % prime == 0) {
+        for (int prime = 2; number > 1; prime++) {
+            for (; number % prime == 0; number /= prime) {
                 primes.add(prime);
-                number /= prime;
             }
-            prime++;
         }
         return primes;
     }
