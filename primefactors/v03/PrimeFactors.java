e5a81a7f4d7458e0b0ef3a8e7d44191613d636da
Hana Lee <hanalee07@gmail.com>
2016-07-28 16:03:39 -0500

    Write test for factoring 1 and set up basic code to handle it

---
+++ primefactors/v03/PrimeFactors.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class PrimeFactors {
+
+    public static ArrayList<Integer> generate(int number) {
+        return new ArrayList<Integer>();
+    }
+
+}
---
+++ primefactors/v03/PrimeFactorsTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+import java.util.ArrayList;
+
+public class PrimeFactorsTest {
+
+    private ArrayList<Integer> list(int... ints) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i : ints) {
+            list.add(i);
+        }
+        return list; 
+    }
+
+    @Test
+    public void testOne() {
+        assertEquals(list(), PrimeFactors.generate(1));
+    } 
+
+}
