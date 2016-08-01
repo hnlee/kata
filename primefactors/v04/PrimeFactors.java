7c966aee98fd90944da40ff3917bee9a637f53cc
Hana Lee <hanalee07@gmail.com>
2016-08-01 16:14:36 -0500

    Refactor while loops to for loops

--- primefactors/v04/PrimeFactors.java
+++ primefactors/v04/PrimeFactors.java
@@ -4,16 +4,10 @@ public class PrimeFactors {
 
     public static ArrayList<Integer> generate(int number) {
         ArrayList<Integer> primes = new ArrayList<Integer>();
-        int prime = 2;
-        while (number > 1) {
-            while (number % prime == 0) {
+        for (int prime = 2; number > 1; prime++) {
+            for (; number % prime == 0; number /= prime) {
                 primes.add(prime);
-                number /= prime;
             }
-            prime++;
-        }
-        if (number > 1) {
-            primes.add(number);
         }
         return primes;
     }
