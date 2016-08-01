7f7e34b88f47764f4799c9e910bbf8371261477b
Hana Lee <hanalee07@gmail.com>
2016-08-01 16:05:47 -0500

    Test and implement factoring 1, 2 and 3

---
+++ primefactors/v04/PrimeFactors.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class PrimeFactors {
+
+    public static ArrayList<Integer> generate(int number) {
+        ArrayList<Integer> primes = new ArrayList<Integer>();
+        if (number > 1) {
+            primes.add(number);
+        }
+        return primes;
+    }
+}
---
+++ primefactors/v04/PrimeFactorsTest.java
@@ -0,0 +1,30 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+import java.util.ArrayList;
+
+public class PrimeFactorsTest {
+
+    private ArrayList<Integer> list(int... ints) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i : ints) {
+            list.add(i);
+        }
+        return list;
+    }
+
+    @Test
+    public void testOne() {
+        assertEquals(list(), PrimeFactors.generate(1));
+    }
+
+    @Test
+    public void testTwo() {
+        assertEquals(list(2), PrimeFactors.generate(2));
+    }
+
+    @Test
+    public void testThree() {
+        assertEquals(list(3), PrimeFactors.generate(3));
+    }
+
+}
