fab90355f73e03339ad0129ba92c70532831396d
Hana Lee <hanalee07@gmail.com>
2016-08-01 16:07:19 -0500

    Test and implement factoring 4

--- primefactors/v04/PrimeFactors.java
+++ primefactors/v04/PrimeFactors.java
@@ -4,6 +4,10 @@ public class PrimeFactors {
 
     public static ArrayList<Integer> generate(int number) {
         ArrayList<Integer> primes = new ArrayList<Integer>();
+        if (number % 2 == 0) {
+            primes.add(2);
+            number /= 2;
+        }
         if (number > 1) {
             primes.add(number);
         }
