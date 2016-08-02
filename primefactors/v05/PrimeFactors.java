6522dc0a4ccf98d825a6de2c68829c12c06ca71e
Hana Lee <hanalee07@gmail.com>
2016-08-02 11:29:55 -0500

    Practice kata for fifth time

---
+++ primefactors/v05/PrimeFactors.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class PrimeFactors {
+
+    public static ArrayList<Integer> generate(int number) {
+        ArrayList<Integer> primes = new ArrayList<Integer>();
+        for (int prime = 2; number > 1; prime++) {
+            for (; number % prime == 0; number /= prime) {
+                primes.add(prime);
+            }
+        }
+        return primes;
+    }
+
+}
