386677765a92422a7193c69b5862ae918f89494c
Hana Lee <hanalee07@gmail.com>
2016-08-09 10:57:20 -0500

    Run through of kata

---
+++ primefactors/v06/PrimeFactorsTest.java
@@ -0,0 +1,50 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+
+public class PrimeFactorsTest {
+
+    private ArrayList<Integer> list(int... ints) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i : ints) {
+            list.add(i);
+        }
+        return list;
+    }
+
+    @Test
+    public void testOne() {
+        assertEquals(list(), PrimeFactors.generate(1));
+    }
+
+    @Test
+    public void testTwo() {
+        assertEquals(list(2), PrimeFactors.generate(2));
+    }
+
+    @Test
+    public void testThree() {
+        assertEquals(list(3), PrimeFactors.generate(3));
+    }
+
+    @Test
+    public void testFour() {
+        assertEquals(list(2, 2), PrimeFactors.generate(4));
+    }
+
+    @Test
+    public void testSix() {
+        assertEquals(list(2, 3), PrimeFactors.generate(6));
+    }
+
+    @Test
+    public void testEight() {
+        assertEquals(list(2, 2, 2), PrimeFactors.generate(8));
+    }
+    
+    @Test
+    public void testNine() {
+        assertEquals(list(3, 3), PrimeFactors.generate(9));
+    }
+
+}
