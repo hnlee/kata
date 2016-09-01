3f928c4d0f643399dbeb7674cdf295a0cb8395f1
Hana Lee <hanalee07@gmail.com>
2016-09-01 14:40:35 -0500

    Refactor to test rolls with varying pin numbers

--- bowlinggame/v01/bowling/test/bowling/core_test.clj
+++ bowlinggame/v01/bowling/test/bowling/core_test.clj
@@ -7,19 +7,21 @@
     (is (not (nil? (start-game :g))))))
 
 (defn roll-multiples [game pins]
-  (loop [i 1 acc game]
-    (if (< i 20)
-      (recur (inc i) (roll acc pins))
-      (roll acc pins))))
+  (loop [i 0 acc game]
+    (if (< i (- (count pins) 1))
+      (recur (inc i) (roll acc (nth pins i)))
+      (roll acc (nth pins i)))))
 
 (deftest test-gutter-game
   (testing "Roll games with only zeros"
     (is (= 0 
       (score (roll-multiples (start-game :g) 
-        0))))))
+        (repeat 20 0)))))))
 
 (deftest test-all-ones
   (testing "Roll a game with only ones"
     (is (= 20 
       (score (roll-multiples (start-game :g) 
-        1))))))
+        (repeat 20 1)))))))
+
+        
