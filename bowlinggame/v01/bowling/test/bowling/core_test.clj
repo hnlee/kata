2c62df9c3c55cb7a364b2f69fcab132706ccb0a0
Hana Lee <hanalee07@gmail.com>
2016-09-06 13:38:12 -0500

    Add tests for spare? and strike? functions

--- bowlinggame/v01/bowling/test/bowling/core_test.clj
+++ bowlinggame/v01/bowling/test/bowling/core_test.clj
@@ -22,6 +22,16 @@
     (is (= 20 
       (score (apply roll [] (repeat 20 1)))))))
 
+(deftest test-spare
+  (testing "Detect if frame is a spare"
+    (is (= true
+      (spare? 5 5)))))
+      
+(deftest test-strike
+  (testing "Detect if frame is a strike"
+    (is (= true
+      (strike? 10)))))
+
 (deftest test-one-spare
   (testing "Roll a game with one spare"
     (is (= 16
