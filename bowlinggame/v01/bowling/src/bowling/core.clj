b96b05487a0df0302e680cb63e17a9b6b73a138b
Hana Lee <hanalee07@gmail.com>
2016-09-06 13:34:45 -0500

    Edit whitespace to fit convention for cond

--- bowlinggame/v01/bowling/src/bowling/core.clj
+++ bowlinggame/v01/bowling/src/bowling/core.clj
@@ -13,17 +13,16 @@
 (defn score [game]
   (loop [points 0 acc game frame 0]
     (cond 
-      (= 10 frame)
-        points
-      (strike? (first acc))
-        (recur (+ points 10 (second acc) (nth acc 2))
-               (rest acc)
-               (inc frame))
-      (spare? (first acc) (second acc))
-        (recur (+ points 10 (nth acc 2))
-               (nthrest acc 2)
-               (inc frame))
-      :else 
-        (recur (+ points (first acc) (second acc))
-               (nthrest acc 2)
-               (inc frame)))))
+      (= 10 frame) points
+      (strike? (first acc)) (recur 
+        (+ points 10 (second acc) (nth acc 2))
+        (rest acc)
+        (inc frame))
+      (spare? (first acc) (second acc)) (recur 
+        (+ points 10 (nth acc 2))
+        (nthrest acc 2)
+        (inc frame))
+      :else (recur 
+        (+ points (first acc) (second acc))
+        (nthrest acc 2)
+        (inc frame)))))
